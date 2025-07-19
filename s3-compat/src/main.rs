b27ce19ae7f30ab94d0068e3114c965f957049b4
lunoos <65530077+lunoos@users.noreply.github.com>
2025-07-19 18:00:17 +0530

    Add tokio-based S3 compatibility server

--- s3-compat/src/main.rs
+++ s3-compat/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::{service::make_service_fn, Server};
+use CIAOS::MyStorage;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let svc = MyStorage::new();
+    let router = s3s::server::router(svc);
+    Server::bind(&([0, 0, 0, 0], 8000).into())
+        .serve(make_service_fn(|_| async {
+            Ok::<_, hyper::Error>(router.clone())
+        }))
+        .await?;
+    Ok(())
 }
